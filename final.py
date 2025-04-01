5ebcbfd0bc4b8ea7a89302fc3c61ffe676fd4145
afinmh <afinmaulana522@gmail.com>
2025-04-01 14:21:22 +0700

    tambah git ignore

--- final.py
+++ final.py
@@ -26,12 +26,10 @@ vol_range = volume.GetVolumeRange()
 min_vol = vol_range[0]
 max_vol = vol_range[1]
 
-# Inisialisasi MediaPipe Hands
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.7)
 mp_draw = mp.solutions.drawing_utils
 
-# Muat model dari file
 model_filename = 'model_fixed.pkl'
 knn = joblib.load(model_filename)
 gesture_label = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K', 11: 'L', 12: 'M', 13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R', 18: 'S', 19: 'T', 20: 'U', 21: 'V', 22: 'W', 23: 'X', 24: 'Y', 25: 'Z', 26: ' ', 27: 'next'}
@@ -43,7 +41,7 @@ def calculate_distance(point1, point2):
 class ShowImage(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(ShowImage, self).__init__()
-        self.setupUi(self)  # ✅ Ganti loadUi dengan setupUi
+        self.setupUi(self) 
         self.Image = None
         self.capture_image = None
         self.cap = None
